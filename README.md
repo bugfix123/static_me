d3417012375f6eb3da25e9fd406d4e8dbccf9e75
ddd <you@example.com>
2020-08-22 16:49:57 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# static_me
+# static_me
+# static_me
+# static_me
+# static_me
+# static_me
---
+++ push.sh
@@ -0,0 +1,9 @@
+echo "# static_me" >> README.md
+git init
+git add .
+git config --global user.email "you@example.com"
+git config --global user.name "ddd"
+git commit -m "first commit"
+git remote remove origin
+git remote add origin https://bugfix123:dz19870412123@github.com/bugfix123/static_me.git
+git push -u origin master
